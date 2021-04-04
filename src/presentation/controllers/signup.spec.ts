4251168bf43e974efbf0afea964f69ff609506d1
romulofss <romulo.fernando@paytrack.com.br>
2021-04-04 20:33:06 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import SignUpController from "./signup";
+describe("SignUp Controller", () => {
+  test("Shold return 400 if no name is provided", () => {
+    // sut significa (S)istem (U)nder (T)est
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        /* name: "any_name", */
+        email: "email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
