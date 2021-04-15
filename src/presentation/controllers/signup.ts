9b24d1699dbdeb542091c2a689e57fdbe51cb4af
romulofss <romulo.fernando@paytrack.com.br>
2021-04-15 17:50:02 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,13 @@
 import { HttpRequest, HttpResponse } from './../protocols/http';
-import {MissingParamError} from "./../errors/missing-param-error"
-
-
-
+import { MissingParamError } from "./../errors/missing-param-error";
+import { badRequest } from '../helpers/http-helper';
 export default class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new MissingParamError("email") };
+      return badRequest(new MissingParamError("email"));
     }
-    if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new MissingParamError("name") };
+      if (!httpRequest.body.name) {
+        return badRequest(new MissingParamError("name"));
     }
   }
 }
