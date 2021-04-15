9878ac6d0d6fd1d380eeb318bc77ac2205ce855f
romulofss <romulo.fernando@paytrack.com.br>
2021-04-15 20:21:22 -0300

    refactor: Move emailValidator creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,15 +7,31 @@ interface SutTypes {
     emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
-    // Stub = Dublê de teste
+const makeEmailValidator = (): EmailValidator => {
+   // Stub = Dublê de teste
     class EmailValidatorStub implements EmailValidator {
         isValid(email: string): boolean {
             return true;
         }
     }
 
-    const emailValidatorStub = new EmailValidatorStub();
+    return new EmailValidatorStub();
+}
+
+const makeEmailValidatorWithError = (): EmailValidator => {
+   // Stub = Dublê de teste
+    class EmailValidatorStub implements EmailValidator {
+        isValid(email: string): boolean {
+           throw new Error();
+        }
+    }
+
+    return new EmailValidatorStub();
+}
+
+const makeSut = (): SutTypes => {
+
+    const emailValidatorStub = makeEmailValidator();
     const sut = new SignUpController(emailValidatorStub);
 
     return {
@@ -112,13 +128,8 @@ describe("SignUp Controller", () => {
 
   test("Should return 500 if emailValidator throws", () => {
     // Stub = Dublê de teste
-    class EmailValidatorStub implements EmailValidator {
-        isValid(email: string): boolean {
-            throw new Error();
-        }
-    }
 
-    const emailValidatorStub = new EmailValidatorStub();
+    const emailValidatorStub = makeEmailValidatorWithError();
     const sut = new SignUpController(emailValidatorStub);
 
     const httpRequest = {
