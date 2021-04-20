fdb0c8424e87fd2a829fa1bb03898a5d4bfbe383
Romulo Fernando <fssromulo@gmail.com>
2021-04-20 14:32:08 -0300

    feature: add account

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -21,7 +21,7 @@ export default class SignUpController implements Controller {
                 }
             }
 
-            const { email, password, passwordConfirmation } = httpRequest.body;
+            const { name, email, password, passwordConfirmation } = httpRequest.body;
             if (password !== passwordConfirmation) {
                 return badRequest(new InvalidParamError("passwordConfirmation"));
             }
@@ -30,7 +30,12 @@ export default class SignUpController implements Controller {
             if (!isEmailValid) {
                 return badRequest(new InvalidParamError("email"));
             }
-
+            
+            this.addAccount.add({
+                name,
+                email,
+                password
+            });
 
         } catch (error) {
             return serverError()
